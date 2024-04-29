963e2a33e8f7c419d6170ba2bb60e43648eaaee5
Barış Sucuoğlu <barissucuoglu34@gmail.com>
2024-04-29 14:04:27 +0300

    Initial Commit

---
+++ Twitter-Clone/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Twitter-Clone
+//
+//  Created by Barış Sucuoğlu on 29.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+
+}
+
